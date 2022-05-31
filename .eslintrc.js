56fbd289c850bfdfcc86a6a4ab0f394768a79514
Andriy Khomenko <andriy.khomenko@intermodaltelematics.com>
2022-05-31 12:57:29 +0100

    fix linter issue

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,7 +10,6 @@ module.exports = {
     "plugin:@typescript-eslint/recommended",
     "plugin:@typescript-eslint/recommended-requiring-type-checking",
     "prettier",
-    "prettier/@typescript-eslint"
   ],
   "parser": "@typescript-eslint/parser",
   "parserOptions": {
